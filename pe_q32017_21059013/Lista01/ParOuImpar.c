423f9c660430de68a0ad31689ac7323be7d2d42e
Rafael <c.rafael@aluno.ufabc.edu.br>
2017-09-22 09:55:58 -0300

    Exercicio 01

---
+++ pe_q32017_21059013/Lista01/ParOuImpar.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+
+int main (){
+	int number;
+
+	scanf("Digite um valor inteiro: %d \n", number);
+
+	if(number%2==0){
+	 printf("Par");
+	}
+	else{
+	printf("Impar");
+	}
+}
+
