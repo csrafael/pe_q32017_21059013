a2584bdfcc6d9b10b738ef9bb9ae4214b5ac93ba
Rafael <c.rafael@aluno.ufabc.edu.br>
2017-09-29 09:50:38 -0300

    Lab02

---
+++ pe_q32017_21059013/Lab02/pi.c
@@ -0,0 +1,38 @@
+# include <stdio.h>
+#include <math.h>
+//compile: gcc -o programa programa.c -lm
+
+
+long int fat ( int number )
+{
+	long int count = number;
+
+	for ( count; count > 0; count-- ) {
+		
+		number = number * (number -1);		
+	}
+	
+	return number;
+}
+
+double somatorio ( int quant, double number  )
+{
+
+	return 0;
+}
+
+int main(){
+
+	//declarando variaveis
+
+	
+	//entrada de variaveis
+	
+	//Saida
+	
+ 
+	return 0;
+}
+
+
+
